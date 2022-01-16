3b309126e9e11674b8190b73d05a6cc0faf90bd6
ELTEGANI MOHAMED <elteganimohamed@ELTEGANIs-MacBook-Air.local>
2022-01-16 19:39:50 +0400

    add network request and single source of trowth

--- app/src/main/java/com/example/dictionaryapp/feature_dictionary/data/remote/dto/WordInfoDto.kt
+++ app/src/main/java/com/example/dictionaryapp/feature_dictionary/data/remote/dto/WordInfoDto.kt
@@ -1,5 +1,6 @@
 package com.example.dictionaryapp.feature_dictionary.data.remote.dto
 
+import com.example.dictionaryapp.feature_dictionary.data.local.entity.WordInfoEntity
 import com.example.dictionaryapp.feature_dictionary.domain.models.WordInfo
 
 data class WordInfoDto(
@@ -8,8 +9,8 @@ data class WordInfoDto(
     val phonetic: String,
     val word: String
 ){
-   fun toWordInfo(): WordInfo {
-       return WordInfo(
+   fun toWordInfoEntity(): WordInfoEntity {
+       return WordInfoEntity(
            meanings = meanings.map { it.toMeaning() },
            origin = origin,
            phonetic = phonetic,
---
+++ app/src/main/java/com/example/dictionaryapp/feature_dictionary/data/repository/WordInfoRepositoryIml.kt
@@ -0,0 +1,38 @@
+package com.example.dictionaryapp.feature_dictionary.data.repository
+
+import com.example.dictionaryapp.core.util.Resource
+import com.example.dictionaryapp.feature_dictionary.data.local.WordInfoDao
+import com.example.dictionaryapp.feature_dictionary.data.remote.DictionaryApi
+import com.example.dictionaryapp.feature_dictionary.domain.models.WordInfo
+import com.example.dictionaryapp.feature_dictionary.domain.repository.WordInfoRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+import java.io.IOException
+
+class WordInfoRepositoryIml(
+    private val dictionaryApi: DictionaryApi,
+    private val wordInfoDao: WordInfoDao
+) : WordInfoRepository{
+    override fun getWordInfo(word: String): Flow<Resource<List<WordInfo>>> = flow{
+        emit(Resource.Loading())
+        val wordsInfo = wordInfoDao.getWordInfo(word).map { it.toWordInfo() }
+        emit(Resource.Loading(data = wordsInfo))
+        try {
+         val remoteWordInfo = dictionaryApi.getWordInfo(word)
+         wordInfoDao.deleteWordInfos(remoteWordInfo.map { it.word })
+         wordInfoDao.insertWordInfos(remoteWordInfo.map { it.toWordInfoEntity() })
+        }catch (httpException:HttpException){
+          emit(Resource.Error(
+              message = "Oops.. something went Wrong",
+              data = wordsInfo
+          ))
+        }catch (ioException:IOException){
+            emit(Resource.Error(
+                message = "Couldn't Reach Server..Please Check Your Internet Connection",
+                data = wordsInfo
+            ))
+        }
+    }
+
+}
